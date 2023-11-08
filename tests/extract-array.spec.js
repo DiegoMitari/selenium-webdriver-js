21e00168607ee2506d135389a232934e44ff1875
diegomitari <diego89ma@gmail.com>
2023-11-07 19:34:43 -0500

    add function:  getSauceDemo

--- tests/extract-array.spec.js
+++ tests/extract-array.spec.js
@@ -1,22 +1,30 @@
-const {Builder, By } = require('selenium-webdriver')
+const { getSauceDemo } = require('./login.spec');
+const {Builder, By } = require('selenium-webdriver');
+
 
 describe('Get an array of products from Sauce Demo Web', () => {
-  it('Get array', async () => {
-    let driver = new Builder()
-                     .forBrowser('chrome')
-                     .build()
+  let driver;
+
+  it('Open the website', async () => {
+    driver = await getSauceDemo();
+  });
+
+  it('Get array from articles inventory', async () => {
+    // let driver = new Builder()
+    //                  .forBrowser('chrome')
+    //                  .build()
     try {
-      await driver.get('https://www.saucedemo.com/');
+      //await driver.get('https://www.saucedemo.com/');
 
       //Login SauceDemo
-      await driver.findElement(By.id('user-name'))
-                  .sendKeys('standard_user')
-      await driver.findElement(By.id('password'))
-                  .sendKeys('secret_sauce')
-      await driver.findElement(By.id('login-button')).click()
-      await driver.sleep(2000)
+      // await driver.findElement(By.id('user-name'))
+      //             .sendKeys('standard_user')
+      // await driver.findElement(By.id('password'))
+      //             .sendKeys('secret_sauce')
+      // await driver.findElement(By.id('login-button')).click()
+      // await driver.sleep(2000)
 
-      //Find the products
+      //Get data from the products
       let elements = await driver.findElements(By.css('.inventory_item'))
       let result = []
       for (const element of elements) {
@@ -27,7 +35,7 @@ describe('Get an array of products from Sauce Demo Web', () => {
           description: rowElement[1]
         })
       }
-      console.log(result)
+      //console.log(result)
     } finally {
       await driver.quit();
     }
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,20 +1,41 @@
 const { Builder, By } = require('selenium-webdriver');
 
-describe('Login Web saucedemo Test', () => {
+async function getSauceDemo() {
 
-  it('Login with standar_user credentials', async () => {
-
-    let driver = new Builder()
-                    .forBrowser('chrome')
-                    .build();
-    await driver.get('https://www.saucedemo.com/');
+  let driver = new Builder()
+                  .forBrowser('chrome')
+                  .build();
+  try {
+    await driver.get('https://www.saucedemo.com/')
 
     await driver.findElement(By.id("user-name"))
                 .sendKeys('standard_user')
     await driver.findElement(By.id('password'))
                 .sendKeys('secret_sauce')
     await driver.findElement(By.id('login-button')).click()
-    await driver.sleep(3000);
-    await driver.quit();
+    await driver.sleep(3000)
+    return driver;
+    /**
+     * El objeto driver es esencial en Selenium, ya que representa la instancia del navegador 
+     * con la que puedes interactuar. Devolver driver permite que el test pueda utilizar este 
+     * objeto en el segundo test para realizar acciones adicionales en la página web abierta.
+     */
+  } catch(error) {
+    console.error('Error al abrir el navegador:', error)
+    return null
+    /**
+     * Devolver null indica que algo salió mal y que el objeto driver no se inicializó correctamente. 
+     * En el segundo test, puedes verificar si driver es null para manejar este caso de error y 
+     * tomar las medidas apropiadas.
+     */
+  }
+};
+
+describe('Login Web Saucedemo Test', () => {
+
+  it('Login with standar_user credentials', () => {
+    getSauceDemo();
   })
 })
+
+module.exports = { getSauceDemo }
